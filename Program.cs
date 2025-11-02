7c3b4d91e3d7af76b79793f534fd8973aaba188d
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:29:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Bouquet
+{
+    public string Flowers;
+    public Bouquet Clone() => (Bouquet)this.MemberwiseClone();
+}
+
+class Program
+{
+    static void Main()
+    {
+        var original = new Bouquet { Flowers = "Троянди " };
+        var copy = original.Clone();
+
+        copy.Flowers = "Тюльпани ";
+
+        Console.WriteLine("Оригінал: " + original.Flowers);
+        Console.WriteLine("Копія: " + copy.Flowers);
+    }
+}
+
+
+
+
+
